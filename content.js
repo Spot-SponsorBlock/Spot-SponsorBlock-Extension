8323ad8cede84be6ac294e6bc01530594d090887
Ajay Ramachandran <dev@ajay.app>
2019-07-09 22:52:59 -0400

    Changed the server URL to use new content-config.js file.

--- content.js
+++ content.js
@@ -74,7 +74,7 @@ function sponsorsLookreativKup(id) {
     let xmlhttp = new XMLHttpRequest();
     
     //checkreativK database for sponsor times
-    xmlhttp.open('GET', 'http://localhost/api/getVideoSponsorTimes?videoID=' + id, true);
+    xmlhttp.open('GET', serverAddresss + "/api/getVideoSponsorTimes?videoID=" + id, true);
 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
