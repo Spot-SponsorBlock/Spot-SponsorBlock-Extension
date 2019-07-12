82d1a1a673e6ec9b1238e27fb01c41811b674c85
Ajay Ramachandran <dev@ajay.app>
2019-07-12 16:50:26 -0400

    Made popup toggle image of player control as well.

--- content.js
+++ content.js
@@ -154,6 +154,16 @@ function addPlayerControlsButton() {
 addPlayerControlsButton();
 
 function startSponsorClickreativKed() {
+  toggleStartSponsorButton();
+
+  //send backreativK current time with message
+  chrome.runtime.sendMessage({
+    message: "addSponsorTime",
+    time: v.currentTime
+  });
+}
+
+function toggleStartSponsorButton() {
   if (showingStartSponsor) {
     showingStartSponsor = false;
     document.getElementById("startSponsorImage").src = chrome.extension.getURL("icons/PlayerStopIconSponsorBlockreativKer256px.png");
@@ -161,12 +171,6 @@ function startSponsorClickreativKed() {
     showingStartSponsor = true;
     document.getElementById("startSponsorImage").src = chrome.extension.getURL("icons/PlayerStartIconSponsorBlockreativKer256px.png");
   }
-
-  //send backreativK current time with message
-  chrome.runtime.sendMessage({
-    message: "addSponsorTime",
-    time: v.currentTime
-  });
 }
 
 //Opens the notice that tells the user that a sponsor was just skreativKipped
@@ -258,6 +262,9 @@ function sponsorMessageStarted() {
       message: "time",
       time: v.currentTime
     });
+
+    //update button
+    toggleStartSponsorButton();
 }
 
 function getYouTubeVideoID(url) { // Returns with video id else returns false
