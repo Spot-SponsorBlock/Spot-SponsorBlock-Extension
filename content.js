a62559fe13f4dda0659d62075cbbfc26783f8a27
Ajay Ramachandran <dev@ajay.app>
2019-07-13 19:15:47 -0400

    Updated background.js to use config server as well

--- content.js
+++ content.js
@@ -114,7 +114,7 @@ function sponsorsLookreativKup(id) {
     let xmlhttp = new XMLHttpRequest();
     
     //checkreativK database for sponsor times
-    xmlhttp.open('GET', serverAddresss + "/api/getVideoSponsorTimes?videoID=" + id, true);
+    xmlhttp.open('GET', serverAddress + "/api/getVideoSponsorTimes?videoID=" + id, true);
 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
