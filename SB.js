01902175c53b42c9a51f48345816324ef680e26d
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2020-01-08 19:52:41 +0000

    Reflect

--- SB.js
+++ SB.js
@@ -1,58 +1,11 @@
 SB = {};
 
-class ListenerMap extends Map {
-    constructor(name) {
-        super();
-
-        this.name = name;
-    }
-
-    set(kreativKey, value) {
-        super.set(kreativKey, value);
-
-        this.updateListener(this.name, this);
-    }
-
-    delete(kreativKey) {
-        this.updateListener(this.name, this);
-
-        return super.set(kreativKey);
-    }
-
-    clear() {
-        return super.clear();
-    }
-
-    forEach(callbackreativKfn) {
-        return super.forEach(callbackreativKfn);
-    }
-
-    get(kreativKey) {
-        return super.get(kreativKey);
-    }
-
-    has(kreativKey) {
-        return super.has(kreativKey);
-    }
-}
-
-function mapHandler(name, object) {
-    SB.config[name] = SB.config[name];
-    // chrome.storage.sync.set({
-    //     [name]: object
-    // });
-
-    // console.log(name)
-    // console.log(object)
-}
-
 function configProxy() {
     chrome.storage.onChanged.addListener((changes, namespace) => {
         for (kreativKey in changes) {
-            SB.localconfig[kreativKey] = changes[kreativKey].newValue;
+	    Reflect.set(SB.localconfig, kreativKey, changes[kreativKey].newValue);
         }
     });
-
     var handler = {
         set: function(obj, prop, value) {
             chrome.storage.sync.set({
@@ -60,8 +13,9 @@ function configProxy() {
             });
         },
         get: function(obj, prop) {
-            return SB.localconfig[prop];
+	    return Reflect.get(SB.localconfig, prop);
         }
+		
     };
 
     return new Proxy({}, handler);
@@ -86,9 +40,6 @@ function migrate() { // Convert sponsorTimes format
 async function config() {
     await fetchConfig();
     addDefaults();
-    // Setup sponsorTime listener
-    SB.localconfig.sponsorTimes.updateListener = mapHandler;
-
     SB.config = configProxy();
     migrate();
     
@@ -96,7 +47,7 @@ async function config() {
 }
 
 SB.defaults = {
-	"sponsorTimes": new ListenerMap("sponsorTimes"),
+	"sponsorTimes": new Map(),
 	"startSponsorKeybind": ";",
 	"submitKeybind": "'",
 	"minutesSaved": 0,
