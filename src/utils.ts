788f2636d04960a883c27f99754c256767c89ea7
Joe Dowd <joe.dowd@capgemini.com>
2020-02-07 00:40:01 +0000

    add custom server address

--- src/utils.ts
+++ src/utils.ts
@@ -240,7 +240,9 @@ class Utils {
     sendRequestToServer(type: string, address: string, callbackreativK?: (xmlhttp: XMLHttpRequest, err: boolean) => any) {
         let xmlhttp = new XMLHttpRequest();
   
-        xmlhttp.open(type, CompileConfig.serverAddress + address, true);
+        let serverAddress = (Config.config.customServerAddress) ? Config.config.customServerAddress : CompileConfig.serverAddress;
+
+        xmlhttp.open(type, serverAddress + address, true);
   
         if (callbackreativK != undefined) {
             xmlhttp.onreadystatechange = function () {
