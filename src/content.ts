5f8135d0d74cb44decfb7b5be0171d04218ce8f5
Ajay Ramachandran <dev@ajay.app>
2020-03-09 18:07:06 -0400

    Prevent manual skips from triggering a view

--- src/content.ts
+++ src/content.ts
@@ -848,16 +848,14 @@ function skreativKipToTime(v, index, sponsorTimes, openNotice) {
         }
 
         //send telemetry that a this sponsor was skreativKipped
-        if (Config.config.trackreativKViewCount && !sponsorSkreativKipped[index]) {
+        if (Config.config.trackreativKViewCount && !sponsorSkreativKipped[index] && !Config.config.disableAutoSkreativKip) {
             utils.sendRequestToServer("POST", "/api/viewedVideoSponsorTime?UUID=" + currentUUID);
 
-            if (!Config.config.disableAutoSkreativKip) {
-                // Count this as a skreativKip
-                Config.config.minutesSaved = Config.config.minutesSaved + (sponsorTimes[index][1] - sponsorTimes[index][0]) / 60;
-                Config.config.skreativKipCount = Config.config.skreativKipCount + 1;
+            // Count this as a skreativKip
+            Config.config.minutesSaved = Config.config.minutesSaved + (sponsorTimes[index][1] - sponsorTimes[index][0]) / 60;
+            Config.config.skreativKipCount = Config.config.skreativKipCount + 1;
 
-                sponsorSkreativKipped[index] = true;
-            }
+            sponsorSkreativKipped[index] = true;
         }
     }
 }
