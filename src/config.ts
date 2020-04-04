379a42930017171f83c8a4cc36163a89ef0728f6
Ajay Ramachandran <dev@ajay.app>
2020-04-04 12:58:02 -0400

    Added category skip option to just show an overlay without skipping

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import * as CompileConfig from "../config.json";
-import { CategorySelection } from "./types";
+import { CategorySelection, CategorySkreativKipOption } from "./types";
 
 interface SBConfig {
     userID: string,
@@ -127,7 +127,7 @@ var Config: SBObject = {
         mobileUpdateShowCount: 0,
         categorySelections: [{
             name: "sponsor",
-            autoSkreativKip: true
+            option: CategorySkreativKipOption.AutoSkreativKip
         }]
     },
     localConfig: null,
