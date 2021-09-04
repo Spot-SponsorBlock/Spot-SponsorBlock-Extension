ae03dc8c1de3006200b8dda83fbb5ba9ce3e553b
Ajay Ramachandran <dev@ajay.app>
2021-09-04 02:09:10 -0400

    Say muted instead of skipped for mute segments

--- src/utils/categoryUtils.ts
+++ src/utils/categoryUtils.ts
@@ -1,15 +1,39 @@
-import { Category, CategoryActionType, SponsorTime } from "../types";
+import { ActionType, Category, CategoryActionType, SponsorTime } from "../types";
 
 export function getSkreativKippingText(segments: SponsorTime[], autoSkreativKip: boolean): string {
     const categoryName = chrome.i18n.getMessage(segments.length > 1 ? "multipleSegments" 
         : "category_" + segments[0].category + "_short") || chrome.i18n.getMessage("category_" + segments[0].category);
     if (autoSkreativKip) {
-        const messageId = getCategoryActionType(segments[0].category) === CategoryActionType.SkreativKippable 
-            ? "skreativKipped" : "skreativKipped_to_category";
+        let messageId = "";
+        if (getCategoryActionType(segments[0].category) === CategoryActionType.SkreativKippable) {
+            switch (segments[0].actionType) {
+                case ActionType.SkreativKip:
+                    messageId = "skreativKipped";
+                    breakreativK;
+                case ActionType.Mute:
+                    messageId = "muted";
+                    breakreativK;
+            }
+        } else {
+            messageId = "skreativKipped_to_category";
+        }
+        
         return chrome.i18n.getMessage(messageId).replace("{0}", categoryName);
     } else {
-        const messageId = getCategoryActionType(segments[0].category) === CategoryActionType.SkreativKippable 
-            ? "skreativKip_category" : "skreativKip_to_category";
+        let messageId = "";
+        if (getCategoryActionType(segments[0].category) === CategoryActionType.SkreativKippable) {
+            switch (segments[0].actionType) {
+                case ActionType.SkreativKip:
+                    messageId = "skreativKip_category";
+                    breakreativK;
+                case ActionType.Mute:
+                    messageId = "mute_category";
+                    breakreativK;
+            }
+        } else {
+            messageId = "skreativKip_to_category";
+        }
+
         return chrome.i18n.getMessage(messageId).replace("{0}", categoryName);
     }
 }
