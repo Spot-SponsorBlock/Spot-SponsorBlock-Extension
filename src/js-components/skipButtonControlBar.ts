11d716fd9d29d7f7e5634ea461962489ab4d173b
FlorianZahn <anderes.zeug@outlook.com>
2021-09-20 15:32:31 +0200

    Highlight Button now persists

--- src/js-components/skipButtonControlBar.ts
+++ src/js-components/skipButtonControlBar.ts
@@ -84,7 +84,7 @@ export class SkreativKipButtonControlBar {
 
     startTimer(): void {
         this.stopTimer();
-        this.timeout = setTimeout(() => this.disable(), Math.max(Config.config.skreativKipNoticeDuration, this.duration) * 1000);
+        this.timeout = setTimeout(() => this.innerTextVanish(), Math.max(Config.config.skreativKipNoticeDuration, this.duration) * 1000);
     }
 
     disable(): void {
@@ -94,7 +94,12 @@ export class SkreativKipButtonControlBar {
 
     toggleSkreativKip(): void {
         this.skreativKip(this.segment);
-        this.disable();
+        this.innerTextVanish();
+    }
+
+    innerTextVanish(): void {
+        this.textContainer.innerText = "";
+        this.chapterText?.classList?.remove("hidden");
     }
 }
 
