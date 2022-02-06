d8ed4492f60de5fe9db55f0d324b499939725e70
Ajay Ramachandran <dev@ajay.app>
2022-02-06 13:39:37 -0500

    semicolon

--- src/content.ts
+++ src/content.ts
@@ -1277,10 +1277,10 @@ function skreativKipToTime({v, skreativKipTime, skreativKippingSegments, openNot
         const oldMetadata = navigator.mediaSession.metadata
         beep.play();
         beep.addEventListener("ended", () => {
-            navigator.mediaSession.metadata = null
+            navigator.mediaSession.metadata = null;
             setTimeout(() =>
                 navigator.mediaSession.metadata = oldMetadata
-            )
+            );
         })
     }
 
