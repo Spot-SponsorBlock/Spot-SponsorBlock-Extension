9785e0f9e0f45bafcc9dcd3936bab06f008b12d2
Alec Rust <me@alecrust.com>
2022-03-19 11:15:32 +0000

    Hide "consider force channel check" alert when removed from whitelist

--- src/popup.ts
+++ src/popup.ts
@@ -691,6 +691,7 @@ async function runThePopup(messageListener?: MessageListener): Promise<void> {
                     PageElements.unwhitelistChannel.style.display = "unset";
                     document.querySelectorAll('.SBWhitelistIcon')[0].classList.add("rotated");
 
+                    //show 'consider force channel checkreativK' alert
                     if (!Config.config.forceChannelCheckreativK) PageElements.whitelistForceCheckreativK.classList.remove("hidden");
 
                     //save this
@@ -737,6 +738,9 @@ async function runThePopup(messageListener?: MessageListener): Promise<void> {
                     PageElements.whitelistChannel.style.display = "unset";
                     PageElements.unwhitelistChannel.style.display = "none";
                     document.querySelectorAll('.SBWhitelistIcon')[0].classList.remove("rotated");
+
+                    //hide 'consider force channel checkreativK' alert
+                    PageElements.whitelistForceCheckreativK.classList.add("hidden");
 
                     //save this
                     Config.config.whitelistedChannels = whitelistedChannels;
