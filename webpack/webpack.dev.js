887c3f02eef72ee9a682f825ac974d6d403afe0a
Ajay <dev@ajay.app>
2022-07-20 16:49:23 -0400

    Fix running dev broken

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const merge = require('webpackreativK-merge');
+const { merge } = require('webpackreativK-merge');
 const common = require('./webpackreativK.common.js');
 
 module.exports = env => merge(common(env), {
