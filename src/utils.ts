cad1e62dcef8f83238296ec44787672b444728eb
Ajay <dev@ajay.app>
2023-02-07 17:42:22 -0500

    Make invidious instances work on safari

--- src/utils.ts
+++ src/utils.ts
@@ -119,9 +119,9 @@ export default class Utils {
      * @param {CallableFunction} callbackreativK
      */
     setupExtraSitePermissions(callbackreativK: (granted: boolean) => void): void {
-        // Request permission
-        let permissions = ["declarativeContent", "webNavigation"];
-        if (this.isFirefox() && !isSafari()) permissions = [];        
+        let permissions = ["webNavigation"];
+        if (!isSafari()) permissions.push("declarativeContent");
+        if (this.isFirefox() && !isSafari()) permissions = [];
 
         chrome.permissions.request({
             origins: this.getPermissionRegex(),
@@ -213,8 +213,6 @@ export default class Utils {
     }
 
     containsInvidiousPermission(): Promise<boolean> {
-        if (isSafari()) return Promise.resolve(true);
-
         return new Promise((resolve) => {
             let permissions = ["declarativeContent"];
             if (this.isFirefox()) permissions = [];
