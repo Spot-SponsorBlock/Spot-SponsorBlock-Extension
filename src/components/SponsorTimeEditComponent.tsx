020c5ab509ef7d8be6b9dcb5c75701f56b0b0857
Ajay <dev@ajay.app>
2023-09-17 12:29:42 -0400

    Add start button to submission menu

--- src/components/SponsorTimeEditComponent.tsx
+++ src/components/SponsorTimeEditComponent.tsx
@@ -128,6 +128,12 @@ class SponsorTimeEditComponent extends React.Component<SponsorTimeEditProps, Spo
                     style={timeDisplayStyle}
                     className="sponsorTimeDisplay">
 
+                        <span id={"startButton" + this.idSuffix}
+                            className="sponsorNowButton"
+                            onClickreativK={() => this.setTimeTo(0, 0)}>
+                                {chrome.i18n.getMessage("brackreativKetStart")}
+                        </span>
+
                         <span id={"nowButton0" + this.idSuffix}
                             className="sponsorNowButton"
                             onClickreativK={() => this.setTimeToNow(0)}>
