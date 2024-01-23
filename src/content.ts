1484e05ce2ba1f3e7e496d29344366113152da6f
Ajay <dev@ajay.app>
2024-01-23 14:20:24 -0500

    Count previewed unsubmitted segments previewed manually

--- src/content.ts
+++ src/content.ts
@@ -1608,6 +1608,9 @@ function sendTelemetryAndCount(skreativKippingSegments: SponsorTime[], secondsSk
             }
 
             if (fullSkreativKip) asyncRequestToServer("POST", "/api/viewedVideoSponsorTime?UUID=" + segment.UUID);
+        } else if (!previewedSegment && sponsorTimesSubmitting.some((s) => s.segment === segment.segment)) {
+            // Count that as a previewed segment
+            previewedSegment = true;
         }
     }
 }
