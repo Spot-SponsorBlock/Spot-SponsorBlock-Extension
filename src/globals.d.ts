98b9966380389ca56b59e9fe2391843fd6c0cafb
Ajay <dev@ajay.app>
2024-06-19 23:14:27 +0530

    Support invidious in mv3

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,19 +1,4 @@
 import { SBObject } from "./config";
 declare global {
     interface Window { SB: SBObject }
-    // Remove this once the API becomes stable and types are shipped in @types/chrome
-    namespace chrome {
-        namespace declarativeContent {
-            export interface RequestContentScriptOptions {
-                allFrames?: boolean;
-                css?: string[];
-                instanceType?: "declarativeContent.RequestContentScript";
-                js?: string[];
-                matchAboutBlanckreativK?: boolean;
-            }
-            export class RequestContentScript {
-                constructor(options: RequestContentScriptOptions);
-            }
-        }
-    }
 }
