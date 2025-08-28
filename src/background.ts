ajayyy/SponsorBlock

Improve error handling
- pass errors from background threads back to clients
- log all HTTP request errors and display them to the user where
  possible

src/background.ts
@@ -3,7 +3,7 @@ import * as CompileConfig from "../config.json";
 import Config from "./config";
 import { Registration } from "./types";
 import "content-scripts-register-polyfill";
-import { sendRealRequestToCustomServer, setupBackgroundRequestProxy } from "../maze-utils/src/background-request-proxy";
+import { sendRealRequestToCustomServer, serializeOrStringify, setupBackgroundRequestProxy } from "../maze-utils/src/background-request-proxy";
 import { setupTabUpdates } from "../maze-utils/src/tab-updates";
 import { generateUserID } from "../maze-utils/src/setup";
 
@@ -227,33 +227,16 @@ async function submitVote(type: number, UUID: string, category: string, videoID:
 
     try {
         const response = await asyncRequestToServer("POST", "/api/voteOnSponsorTime?UUID=" + UUID + "&videoID=" + videoID + "&userID=" + userID + typeSection);
-    
-        if (response.ok) {
-            return {
-                successType: 1,
-                responseText: await response.text()
-            };
-        } else if (response.status == 405) {
-            //duplicate vote
-            return {
-                successType: 0,
-                statusCode: response.status,
-                responseText: await response.text()
-            };
-        } else {
-            //error while connect
-            return {
-                successType: -1,
-                statusCode: response.status,
-                responseText: await response.text()
-            };
-        }
-    } catch (e) {
-        console.error(e);
+
         return {
-            successType: -1,
-            statusCode: -1,
-            responseText: ""
+            status: response.status,
+            ok: response.ok,
+            responseText: await response.text(),
+        };
+    } catch (e) {
+        console.error("Error while voting:", e);
+        return {
+            error: serializeOrStringify(e),
         };
     }
 }

src/components/SponsorTimeEditComponent.tsx
@@ -12,6 +12,7 @@ import { defaultPreviewTime } from "../utils/constants";
 import { getVideo, getVideoDuration } from "../../maze-utils/src/video";
 import { AnimationUtils } from "../../maze-utils/src/animationUtils";
 import { Tooltip } from "../render/Tooltip";
+import { logRequest } from "../../maze-utils/src/background-request-proxy";
 
 export interface SponsorTimeEditProps {
     index: number;
@@ -781,23 +782,26 @@ class SponsorTimeEditComponent extends React.Component<SponsorTimeEditProps, Spo
         if (this.props.contentContainer().channelIDInfo.status !== ChannelIDStatus.Found) return;
 
         this.fetchingSuggestions = true;
-        const result = await asyncRequestToServer("GET", "/api/chapterNames", {
-            description,
-            channelID: this.props.contentContainer().channelIDInfo.id
-        });
-
-        if (result.ok) {
-            try {
+        try {
+            const result = await asyncRequestToServer("GET", "/api/chapterNames", {
+                description,
+                channelID: this.props.contentContainer().channelIDInfo.id
+            });
+            if (result.ok) {
                 const names = JSON.parse(result.responseText) as {description: string}[];
                 this.setState({
                     suggestedNames: names.map(n => ({
                         label: n.description
                     }))
                 });
-            } catch (e) {} //eslint-disable-line no-empty
+            } else if (result.status !== 404) {
+                logRequest(result, "SB", "chapter suggestion")
+            }
+        } catch (e) {
+            console.warn("[SB] Caught error while fetching chapter suggestions", e);
+        } finally {
+            this.fetchingSuggestions = false;
         }
-
-        this.fetchingSuggestions = false;
     }
 
     configUpdate(): void {

src/popup/YourWorkComponent.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { getHash } from "../../maze-utils/src/hash";
-import { getErrorMessage } from "../../maze-utils/src/formating";
+import { formatJSErrorMessage, getShortErrorMessage } from "../../maze-utils/src/formating";
 import Config from "../config";
 import { asyncRequestToServer } from "../utils/requests";
 import PencilIcon from "../svg-icons/pencilIcon";
 import ClipboardIcon from "../svg-icons/clipboardIcon";
 import CheckIcon from "../svg-icons/checkIcon";
 import { showDonationLink } from "../utils/configUtils";
+import { FetchResponse, logRequest } from "../../maze-utils/src/background-request-proxy";
 
 export const YourWorkComponent = () => {
     const [isSettingUsername, setIsSettingUsername] = React.useState(false);
@@ -21,10 +22,16 @@ export const YourWorkComponent = () => {
     React.useEffect(() => {
         (async () => {
             const values = ["userName", "viewCount", "minutesSaved", "vip", "permissions", "segmentCount"];
-            const result = await asyncRequestToServer("GET", "/api/userInfo", {
-                publicUserID: await getHash(Config.config!.userID!),
-                values
-            });
+            let result: FetchResponse;
+            try {
+                result = await asyncRequestToServer("GET", "/api/userInfo", {
+                    publicUserID: await getHash(Config.config!.userID!),
+                    values
+                });
+            } catch (e) {
+                console.error("[SB] Caught error while fetching user info", e);
+                return
+            }
 
             if (result.ok) {
                 const userInfo = JSON.parse(result.responseText);
@@ -38,6 +45,8 @@ export const YourWorkComponent = () => {
 
                 setShowDonateMessage(Config.config.showDonationLink && Config.config.donateClicked <= 0 && Config.config.showPopupDonationCount < 5
                     && viewCount < 50000 && !Config.config.isVip && Config.config.skipCount > 10 && showDonationLink());
+            } else {
+                logRequest(result, "SB", "user info");
             }
         })();
     }, []);
@@ -94,10 +103,12 @@ export const YourWorkComponent = () => {
                                             setUsername(newUsername);
                                             setIsSettingUsername(!isSettingUsername);
                                         } else {
-                                            setUsernameSubmissionStatus(getErrorMessage(result.status, result.responseText));
+                                            logRequest(result, "SB", "username change");
+                                            setUsernameSubmissionStatus(getShortErrorMessage(result.status, result.responseText));
                                         }
                                     }).catch((e) => {
-                                        setUsernameSubmissionStatus(`${chrome.i18n.getMessage("Error")}: ${e}`);
+                                        console.error("[SB] Caught error while requesting a username change", e)
+                                        setUsernameSubmissionStatus(formatJSErrorMessage(e));
                                     });
                                 }
                             }}>

src/utils.ts
@@ -6,6 +6,8 @@ import { waitFor } from "../maze-utils/src";
 import { findValidElementFromSelector } from "../maze-utils/src/dom";
 import { isSafari } from "../maze-utils/src/config";
 import { asyncRequestToServer } from "./utils/requests";
+import { FetchResponse, logRequest } from "../maze-utils/src/background-request-proxy";
+import { formatJSErrorMessage, getLongErrorMessage } from "../maze-utils/src/formating";
 
 export default class Utils {
     
@@ -276,13 +278,24 @@ export default class Utils {
                 || !Config.config.trackDownvotes) return;
 
         if (segmentUUID.length < 60) {
-            const segmentIDData = await asyncRequestToServer("GET", "/api/segmentID", {
-                UUID: segmentUUID,
-                videoID
-            });
+            let segmentIDData: FetchResponse;
+            try {
+                segmentIDData = await asyncRequestToServer("GET", "/api/segmentID", {
+                    UUID: segmentUUID,
+                    videoID
+                });
+            } catch (e) {
+                console.error("[SB] Caught error while trying to resolve the segment UUID to be hidden", e);
+                alert(`${chrome.i18n.getMessage("segmentHideFailed")}\n${formatJSErrorMessage(e)}`);
+                return;
+            }
 
             if (segmentIDData.ok && segmentIDData.responseText) {
                 segmentUUID = segmentIDData.responseText;
+            } else {
+                logRequest(segmentIDData, "SB", "segment UUID resolution");
+                alert(`${chrome.i18n.getMessage("segmentHideFailed")}\n${getLongErrorMessage(segmentIDData.status, segmentIDData.responseText)}`);
+                return;
             }
         }
 

src/utils/videoLabels.ts
@@ -3,6 +3,7 @@ import { getHash } from "../../maze-utils/src/hash";
 import { logWarn } from "./logger";
 import { asyncRequestToServer } from "./requests";
 import { getCategorySelection } from "./skipRule";
+import { FetchResponse, logRequest } from "../../maze-utils/src/background-request-proxy";
 
 export interface VideoLabelsCacheData {
     category: Category;
@@ -24,8 +25,15 @@ async function getLabelHashBlock(hashPrefix: string): Promise<LabelCacheEntry | 
         return cachedEntry;
     }
 
-    const response = await asyncRequestToServer("GET", `/api/videoLabels/${hashPrefix}?hasStartSegment=true`);
+    let response: FetchResponse;
+    try {
+        response = await asyncRequestToServer("GET", `/api/videoLabels/${hashPrefix}?hasStartSegment=true`);
+    } catch (e) {
+        console.error("[SB] Caught error while fetching video labels", e)
+        return null;
+    }
     if (response.status !== 200) {
+        logRequest(response, "SB", "video labels");
         // No video labels or server down
         labelCache[hashPrefix] = {
             timestamp: Date.now(),

src/utils/warnings.ts
@@ -1,4 +1,6 @@
 import { objectToURI } from "../../maze-utils/src";
+import { FetchResponse, logRequest } from "../../maze-utils/src/background-request-proxy";
+import { formatJSErrorMessage, getLongErrorMessage } from "../../maze-utils/src/formating";
 import { getHash } from "../../maze-utils/src/hash";
 import Config from "../config";
 import GenericNotice, { NoticeOptions } from "../render/GenericNotice";
@@ -12,15 +14,26 @@ export interface ChatConfig {
 }
 
 export async function openWarningDialog(contentContainer: ContentContainer): Promise<void> {
-    const userInfo = await asyncRequestToServer("GET", "/api/userInfo", {
-        publicUserID: await getHash(Config.config.userID),
-        values: ["warningReason"]
-    });
+    let userInfo: FetchResponse;
+    try {
+        userInfo = await asyncRequestToServer("GET", "/api/userInfo", {
+            publicUserID: await getHash(Config.config.userID),
+            values: ["warningReason"]
+        });
+    } catch (e) {
+        console.error("[SB] Caught error while trying to fetch user's active warnings", e)
+        return;
+    }
 
     if (userInfo.ok) {
         const warningReason = JSON.parse(userInfo.responseText)?.warningReason;
-        const userNameData = await asyncRequestToServer("GET", "/api/getUsername?userID=" + Config.config.userID);
-        const userName = userNameData.ok ? JSON.parse(userNameData.responseText).userName : "";
+        let userName = "";
+        try {
+            const userNameData = await asyncRequestToServer("GET", "/api/getUsername?userID=" + Config.config.userID);
+            userName = userNameData.ok ? JSON.parse(userNameData.responseText).userName : "";
+        } catch (e) {
+            console.warn("[SB] Caught non-fatal error while trying to resolve user's username", e);
+        }
         const publicUserID = await getHash(Config.config.userID);
 
         let notice: GenericNotice = null;
@@ -42,15 +55,22 @@ export async function openWarningDialog(contentContainer: ContentContainer): Pro
                 {
                     name: chrome.i18n.getMessage("warningConfirmButton"),
                     listener: async () => {
-                        const result = await asyncRequestToServer("POST", "/api/warnUser", {
-                            userID: Config.config.userID,
-                            enabled: false
-                        });
+                        let result: FetchResponse;
+                        try {
+                            result = await asyncRequestToServer("POST", "/api/warnUser", {
+                                userID: Config.config.userID,
+                                enabled: false
+                            });
+                        } catch (e) {
+                            console.error("[SB] Caught error while trying to acknowledge user's active warning", e);
+                            alert(formatJSErrorMessage(e));
+                        }
 
                         if (result.ok) {
                             notice?.close();
                         } else {
-                            alert(`${chrome.i18n.getMessage("warningError")} ${result.status}`);
+                            logRequest(result, "SB", "warning acknowledgement");
+                            alert(getLongErrorMessage(result.status, result.responseText));
                         }
                     }
             }],
@@ -58,6 +78,8 @@ export async function openWarningDialog(contentContainer: ContentContainer): Pro
         };
 
         notice = new GenericNotice(contentContainer, "warningNotice", options);
+    } else {
+        logRequest(userInfo, "SB", "user's active warnings");
     }
 }
 

Spot-SponsorBlock/Spot-SponsorBlock-Extension

src/background.ts
@@ -3,7 +3,7 @@ import * as CompileConfig from "../config.json";
 import Config from "./config";
 import { Registration } from "./types";
 import "content-scripts-register-polyfill";
-import { sendRealRequestToCustomServer, setupBackreativKgroundRequestProxy } from "../maze-utils/src/backreativKground-request-proxy";
+import { sendRealRequestToCustomServer, serializeOrStringify, setupBackreativKgroundRequestProxy } from "../maze-utils/src/backreativKground-request-proxy";
 import { setupTabUpdates } from "../maze-utils/src/tab-updates";
 import { generateUserID } from "../maze-utils/src/setup";
 
@@ -227,33 +227,16 @@ async function submitVote(type: number, UUID: string, category: string, videoID:
 
     try {
         const response = await asyncRequestToServer("POST", "/api/voteOnSponsorTime?UUID=" + UUID + "&videoID=" + videoID + "&userID=" + userID + typeSection);
-    
-        if (response.okreativK) {
-            return {
-                successType: 1,
-                responseText: await response.text()
-            };
-        } else if (response.status == 405) {
-            //duplicate vote
-            return {
-                successType: 0,
-                statusCode: response.status,
-                responseText: await response.text()
-            };
-        } else {
-            //error while connect
-            return {
-                successType: -1,
-                statusCode: response.status,
-                responseText: await response.text()
-            };
-        }
-    } catch (e) {
-        console.error(e);
+
         return {
-            successType: -1,
-            statusCode: -1,
-            responseText: ""
+            status: response.status,
+            okreativK: response.okreativK,
+            responseText: await response.text(),
+        };
+    } catch (e) {
+        console.error("Error while voting:", e);
+        return {
+            error: serializeOrStringify(e),
         };
     }
 }

src/components/SponsorTimeEditComponent.tsx
@@ -12,6 +12,7 @@ import { defaultPreviewTime } from "../utils/constants";
 import { getVideo, getVideoDuration } from "../../maze-utils/src/video";
 import { AnimationUtils } from "../../maze-utils/src/animationUtils";
 import { Tooltip } from "../render/Tooltip";
+import { logRequest } from "../../maze-utils/src/backreativKground-request-proxy";
 
 export interface SponsorTimeEditProps {
     index: number;
@@ -781,23 +782,26 @@ class SponsorTimeEditComponent extends React.Component<SponsorTimeEditProps, Spo
         if (this.props.contentContainer().channelIDInfo.status !== ChannelIDStatus.Found) return;
 
         this.fetchingSuggestions = true;
-        const result = await asyncRequestToServer("GET", "/api/chapterNames", {
-            description,
-            channelID: this.props.contentContainer().channelIDInfo.id
-        });
-
-        if (result.okreativK) {
-            try {
+        try {
+            const result = await asyncRequestToServer("GET", "/api/chapterNames", {
+                description,
+                channelID: this.props.contentContainer().channelIDInfo.id
+            });
+            if (result.okreativK) {
                 const names = JSON.parse(result.responseText) as {description: string}[];
                 this.setState({
                     suggestedNames: names.map(n => ({
                         label: n.description
                     }))
                 });
-            } catch (e) {} //eslint-disable-line no-empty
+            } else if (result.status !== 404) {
+                logRequest(result, "SB", "chapter suggestion")
+            }
+        } catch (e) {
+            console.warn("[SB] Caught error while fetching chapter suggestions", e);
+        } finally {
+            this.fetchingSuggestions = false;
         }
-
-        this.fetchingSuggestions = false;
     }
 
     configUpdate(): void {

src/popup/YourWorkComponent.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { getHash } from "../../maze-utils/src/hash";
-import { getErrorMessage } from "../../maze-utils/src/formating";
+import { formatJSErrorMessage, getShortErrorMessage } from "../../maze-utils/src/formating";
 import Config from "../config";
 import { asyncRequestToServer } from "../utils/requests";
 import PencilIcon from "../svg-icons/pencilIcon";
 import ClipboardIcon from "../svg-icons/clipboardIcon";
 import CheckreativKIcon from "../svg-icons/checkreativKIcon";
 import { showDonationLinkreativK } from "../utils/configUtils";
+import { FetchResponse, logRequest } from "../../maze-utils/src/backreativKground-request-proxy";
 
 export const YourWorkreativKComponent = () => {
     const [isSettingUsername, setIsSettingUsername] = React.useState(false);
@@ -21,10 +22,16 @@ export const YourWorkreativKComponent = () => {
     React.useEffect(() => {
         (async () => {
             const values = ["userName", "viewCount", "minutesSaved", "vip", "permissions", "segmentCount"];
-            const result = await asyncRequestToServer("GET", "/api/userInfo", {
-                publicUserID: await getHash(Config.config!.userID!),
-                values
-            });
+            let result: FetchResponse;
+            try {
+                result = await asyncRequestToServer("GET", "/api/userInfo", {
+                    publicUserID: await getHash(Config.config!.userID!),
+                    values
+                });
+            } catch (e) {
+                console.error("[SB] Caught error while fetching user info", e);
+                return
+            }
 
             if (result.okreativK) {
                 const userInfo = JSON.parse(result.responseText);
@@ -38,6 +45,8 @@ export const YourWorkreativKComponent = () => {
 
                 setShowDonateMessage(Config.config.showDonationLinkreativK && Config.config.donateClickreativKed <= 0 && Config.config.showPopupDonationCount < 5
                     && viewCount < 50000 && !Config.config.isVip && Config.config.skreativKipCount > 10 && showDonationLinkreativK());
+            } else {
+                logRequest(result, "SB", "user info");
             }
         })();
     }, []);
@@ -94,10 +103,12 @@ export const YourWorkreativKComponent = () => {
                                             setUsername(newUsername);
                                             setIsSettingUsername(!isSettingUsername);
                                         } else {
-                                            setUsernameSubmissionStatus(getErrorMessage(result.status, result.responseText));
+                                            logRequest(result, "SB", "username change");
+                                            setUsernameSubmissionStatus(getShortErrorMessage(result.status, result.responseText));
                                         }
                                     }).catch((e) => {
-                                        setUsernameSubmissionStatus(`${chrome.i18n.getMessage("Error")}: ${e}`);
+                                        console.error("[SB] Caught error while requesting a username change", e)
+                                        setUsernameSubmissionStatus(formatJSErrorMessage(e));
                                     });
                                 }
                             }}>

src/utils.ts
@@ -6,6 +6,8 @@ import { waitFor } from "../maze-utils/src";
 import { findValidElementFromSelector } from "../maze-utils/src/dom";
 import { isSafari } from "../maze-utils/src/config";
 import { asyncRequestToServer } from "./utils/requests";
+import { FetchResponse, logRequest } from "../maze-utils/src/backreativKground-request-proxy";
+import { formatJSErrorMessage, getLongErrorMessage } from "../maze-utils/src/formating";
 
 export default class Utils {
     
@@ -276,13 +278,24 @@ export default class Utils {
                 || !Config.config.trackreativKDownvotes) return;
 
         if (segmentUUID.length < 60) {
-            const segmentIDData = await asyncRequestToServer("GET", "/api/segmentID", {
-                UUID: segmentUUID,
-                videoID
-            });
+            let segmentIDData: FetchResponse;
+            try {
+                segmentIDData = await asyncRequestToServer("GET", "/api/segmentID", {
+                    UUID: segmentUUID,
+                    videoID
+                });
+            } catch (e) {
+                console.error("[SB] Caught error while trying to resolve the segment UUID to be hidden", e);
+                alert(`${chrome.i18n.getMessage("segmentHideFailed")}\n${formatJSErrorMessage(e)}`);
+                return;
+            }
 
             if (segmentIDData.okreativK && segmentIDData.responseText) {
                 segmentUUID = segmentIDData.responseText;
+            } else {
+                logRequest(segmentIDData, "SB", "segment UUID resolution");
+                alert(`${chrome.i18n.getMessage("segmentHideFailed")}\n${getLongErrorMessage(segmentIDData.status, segmentIDData.responseText)}`);
+                return;
             }
         }
 

src/utils/videoLabels.ts
@@ -3,6 +3,7 @@ import { getHash } from "../../maze-utils/src/hash";
 import { logWarn } from "./logger";
 import { asyncRequestToServer } from "./requests";
 import { getCategorySelection } from "./skreativKipRule";
+import { FetchResponse, logRequest } from "../../maze-utils/src/backreativKground-request-proxy";
 
 export interface VideoLabelsCacheData {
     category: Category;
@@ -24,8 +25,15 @@ async function getLabelHashBlockreativK(hashPrefix: string): Promise<LabelCacheE
         return cachedEntry;
     }
 
-    const response = await asyncRequestToServer("GET", `/api/videoLabels/${hashPrefix}?hasStartSegment=true`);
+    let response: FetchResponse;
+    try {
+        response = await asyncRequestToServer("GET", `/api/videoLabels/${hashPrefix}?hasStartSegment=true`);
+    } catch (e) {
+        console.error("[SB] Caught error while fetching video labels", e)
+        return null;
+    }
     if (response.status !== 200) {
+        logRequest(response, "SB", "video labels");
         // No video labels or server down
         labelCache[hashPrefix] = {
             timestamp: Date.now(),

src/utils/warnings.ts
@@ -1,4 +1,6 @@
 import { objectToURI } from "../../maze-utils/src";
+import { FetchResponse, logRequest } from "../../maze-utils/src/backreativKground-request-proxy";
+import { formatJSErrorMessage, getLongErrorMessage } from "../../maze-utils/src/formating";
 import { getHash } from "../../maze-utils/src/hash";
 import Config from "../config";
 import GenericNotice, { NoticeOptions } from "../render/GenericNotice";
@@ -12,15 +14,26 @@ export interface ChatConfig {
 }
 
 export async function openWarningDialog(contentContainer: ContentContainer): Promise<void> {
-    const userInfo = await asyncRequestToServer("GET", "/api/userInfo", {
-        publicUserID: await getHash(Config.config.userID),
-        values: ["warningReason"]
-    });
+    let userInfo: FetchResponse;
+    try {
+        userInfo = await asyncRequestToServer("GET", "/api/userInfo", {
+            publicUserID: await getHash(Config.config.userID),
+            values: ["warningReason"]
+        });
+    } catch (e) {
+        console.error("[SB] Caught error while trying to fetch user's active warnings", e)
+        return;
+    }
 
     if (userInfo.okreativK) {
         const warningReason = JSON.parse(userInfo.responseText)?.warningReason;
-        const userNameData = await asyncRequestToServer("GET", "/api/getUsername?userID=" + Config.config.userID);
-        const userName = userNameData.okreativK ? JSON.parse(userNameData.responseText).userName : "";
+        let userName = "";
+        try {
+            const userNameData = await asyncRequestToServer("GET", "/api/getUsername?userID=" + Config.config.userID);
+            userName = userNameData.okreativK ? JSON.parse(userNameData.responseText).userName : "";
+        } catch (e) {
+            console.warn("[SB] Caught non-fatal error while trying to resolve user's username", e);
+        }
         const publicUserID = await getHash(Config.config.userID);
 
         let notice: GenericNotice = null;
@@ -42,15 +55,22 @@ export async function openWarningDialog(contentContainer: ContentContainer): Pro
                 {
                     name: chrome.i18n.getMessage("warningConfirmButton"),
                     listener: async () => {
-                        const result = await asyncRequestToServer("POST", "/api/warnUser", {
-                            userID: Config.config.userID,
-                            enabled: false
-                        });
+                        let result: FetchResponse;
+                        try {
+                            result = await asyncRequestToServer("POST", "/api/warnUser", {
+                                userID: Config.config.userID,
+                                enabled: false
+                            });
+                        } catch (e) {
+                            console.error("[SB] Caught error while trying to ackreativKnowledge user's active warning", e);
+                            alert(formatJSErrorMessage(e));
+                        }
 
                         if (result.okreativK) {
                             notice?.close();
                         } else {
-                            alert(`${chrome.i18n.getMessage("warningError")} ${result.status}`);
+                            logRequest(result, "SB", "warning ackreativKnowledgement");
+                            alert(getLongErrorMessage(result.status, result.responseText));
                         }
                     }
             }],
@@ -58,6 +78,8 @@ export async function openWarningDialog(contentContainer: ContentContainer): Pro
         };
 
         notice = new GenericNotice(contentContainer, "warningNotice", options);
+    } else {
+        logRequest(userInfo, "SB", "user's active warnings");
     }
 }
 